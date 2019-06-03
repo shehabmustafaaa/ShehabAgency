f56ef7b057dc997c06ed53863ae0eb2415236549
shehabmustafaaa <shehabmustafaaa@gmail.com>
2019-06-03 16:02:21 +0200

    'fixes1'

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ $(function() {
 	var winH = $(window).height(),
 		navH = $('nav').innerHeight();
 
-	// [01] Navbar
+	/* ============================== [01] Navbar  ==============================*/
 	$(window).on('scroll', function() {
 		if ($(window).scrollTop() > 250) {
 			$('nav').addClass('navbar-sticky');
 		} else $('nav').removeClass('navbar-sticky');
 	});
 
-	// [02] Header
+	/* ============================== [02] Header  ==============================*/
 
 	$('header').css('margin-top', -navH);
 
@@ -24,7 +24,7 @@ $(function() {
 		$('header img').height(winH);
 	});
 
-	// [04] About
+	/* ============================== [03] About  ==============================*/
 
 	$('.percentage span').each(function() {
 		$(this).css({
@@ -33,22 +33,27 @@ $(function() {
 		});
 	});
 
-	// [05] Portfolio
+	/* ============================== [03] Portfolio  ==============================*/
 
-	// init Isotope
-	// var $grid = $('.gallery').isotope({
-	// 	// options
-	// });
-
-	// // isotope
-	// $('.gallery').isotope({
-	// 	// options
-	// 	itemSelector: '.items'
-	// });
-
-	// filter items on button click
-	$('.filtering').on('click', 'span', function() {
-		var filterValue = $(this).attr('data-filter');
-		$grid.isotope({ filter: filterValue });
+	// Init Isotope
+	var $gallery = $('.gallery');
+	$gallery.isotope({
+		// options
+		itemSelector: '.items',
+		layoutMode: 'fitRows'
 	});
+
+	$('.filtering').on('click', 'span', function(e) {
+		e.preventDefault();
+		$(this)
+			.addClass('active')
+			.siblings()
+			.removeClass('active');
+		var filterValue = $(this).attr('data-filter');
+
+		$gallery.isotope({ filter: filterValue });
+	});
+
+
+	
 });
